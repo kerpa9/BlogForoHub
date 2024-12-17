fc613b07886a5385d9c89c10638c46db013603e8
Kevin Reyes <kerpa9@gmail.com>
2024-12-17 12:20:28 -0500

    Final created entities

--- src/main/java/ForoHub/Blog/Domain/Models/Profile.java
+++ src/main/java/ForoHub/Blog/Domain/Models/Profile.java
@@ -24,6 +24,6 @@ public class Profile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String name_profile;
 
 }
---
+++ src/main/resources/db/migration/V6__create-table-profile.sql
@@ -0,0 +1,10 @@
+
+create table profile(
+        
+        id bigint not null auto_increment,
+        name_profile varchar(100) not null,
+     
+
+        primary key(id)
+
+);
