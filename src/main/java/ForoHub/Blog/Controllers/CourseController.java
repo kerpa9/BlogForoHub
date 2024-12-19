1972d53fb3b8e36baa9f98e4c6a46125af4128f2
Kevin Reyes <kerpa9@gmail.com>
2024-12-19 11:49:07 -0500

    Insert data into database

--- src/main/java/ForoHub/Blog/Controllers/CourseController.java
+++ src/main/java/ForoHub/Blog/Controllers/CourseController.java
@@ -1,8 +1,33 @@
 package ForoHub.Blog.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ForoHub.Blog.Domain.DTOs.CourseDTO;
+import ForoHub.Blog.Domain.Models.Course;
+import ForoHub.Blog.Services.CourseService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+
 @RestController
+@RequestMapping("/api/v1/course")
+@SecurityRequirement(name = "bearer-key")
 public class CourseController {
-    
+
+    @Autowired
+    private CourseService courseService;
+
+    @PostMapping
+    public ResponseEntity<Course> createCourse(@RequestBody @Valid CourseDTO courseDTO) {
+
+        Course createCourse = courseService.createCourse(courseDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createCourse);
+    }
+
 }
--- src/main/java/ForoHub/Blog/Services/CourseService.java
+++ src/main/java/ForoHub/Blog/Services/CourseService.java
@@ -1,8 +1,27 @@
 package ForoHub.Blog.Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ForoHub.Blog.Domain.DTOs.CourseDTO;
+import ForoHub.Blog.Domain.Models.Course;
+import ForoHub.Blog.Repository.CourseRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 @Service
 public class CourseService {
-    
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Transactional
+    public Course createCourse(@Valid CourseDTO courseDTO) {
+        Course course = new Course();
+        course.setName(courseDTO.name());
+        course.setCategory(courseDTO.category());
+        
+        return courseRepository.save(course);
+    }
+
 }
